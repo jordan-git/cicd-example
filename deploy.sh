69317719948612715b8835611df472a5da8254c0
Jordan <jordanquinlan734@gmail.com>
2025-02-27 13:22:57 +0000

    Adjusted deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 sudo apt update && sudo apt install -y nodejs npm
 sudo npm install -g pm2
 pm2 stop cicd_example
-cd cicd_example
+cd ./cicd_example
 npm install
 pm2 start ./bin/www --name cicd_example
